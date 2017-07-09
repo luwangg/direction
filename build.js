e6c54ecd5714f54209de9496bbefa8948f318f28
Titus Wormer <tituswormer@gmail.com>
2017-07-09 11:42:37 +0200

    Rebuild

--- build.js
+++ build.js
@@ -20,36 +20,18 @@ function oninputchange() {
 }
 
 },{"direction":2}],2:[function(require,module,exports){
-/**
- * @author Titus Wormer
- * @copyright 2014 Titus Wormer
- * @license MIT
- * @module direction
- * @fileoverview Detect directionality: left-to-right,
- *   right-to-left, or neutral.
- */
-
 'use strict';
 
-/* Expose. */
 module.exports = direction;
 
-/* Ranges. */
 var RTL = '\u0591-\u07FF\uFB1D-\uFDFD\uFE70-\uFEFC';
 var LTR = 'A-Za-z\u00C0-\u00D6\u00D8-\u00F6' +
   '\u00F8-\u02B8\u0300-\u0590\u0800-\u1FFF\u200E\u2C00-\uFB1C' +
   '\uFE00-\uFE6F\uFEFD-\uFFFF';
 
-/* Expressions. */
 var rtl = new RegExp('^[^' + LTR + ']*[' + RTL + ']');
 var ltr = new RegExp('^[^' + RTL + ']*[' + LTR + ']');
 
-/**
- * Detect directionality.
- *
- * @param {string} value - Value to check.
- * @return {string} - One of `'rtl'`, `'ltr'`, or `'neutral'`.
- */
 function direction(value) {
   value = String(value || '');
 
